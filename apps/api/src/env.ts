eab892177c92e59a82f04731e94420b01f2b26d1
Joe Shimbi <joeshimbi@gmail.com>
2026-07-31 14:06:44 +0300

    chore(api):environment variables

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -6,7 +6,7 @@ export const env = createEnv({
 		DATABASE_URL: z.url(),
 		BETTER_AUTH_SECRET: z.string().min(1),
 		BETTER_AUTH_URL: z.string().url(),
-		PORT: z.string(),
+		DB_PORT: z.string(),
 	},
 	runtimeEnv: process.env,
 });
