012091225b579367bfc2ea7f148c5ce38974dcaf
Joe Shimbi <joeshimbi@gmail.com>
2026-07-31 17:47:57 +0300

    chore:add port to env var

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -7,6 +7,7 @@ export const env = createEnv({
 		BETTER_AUTH_SECRET: z.string().min(1),
 		BETTER_AUTH_URL: z.string().url(),
 		DB_PORT: z.string(),
+		PORT: z.string().default("8080"),
 	},
 	runtimeEnv: process.env,
 });
